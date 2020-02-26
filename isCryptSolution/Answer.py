8607b9e632ec460b96e7d196784cb0cb146d9f4c
LiamLowndes <36050320+LiamLowndes@users.noreply.github.com>
2020-02-26 17:16:25 -0500

    Update Answer.py

--- isCryptSolution/Answer.py
+++ isCryptSolution/Answer.py
@@ -1,5 +1,7 @@
 def isCryptSolution(crypt, solution):
     combo = []
+    
+    #make combo a list of the three numbers seperated by "-"
     for i in range(3):
         for j in crypt[i]:
             for k in solution:
@@ -10,6 +12,8 @@ def isCryptSolution(crypt, solution):
     
     newCombo = [[]]
     count = 0
+    
+    #break the numbers into their three individual parts and store it in 2D array newCombo
     for i in combo:
         if i == "_":
             newCombo.append([])
@@ -18,15 +22,18 @@ def isCryptSolution(crypt, solution):
             newCombo[count].append(i)
     newCombo.remove([])
     
+    #check for leading zeros
     for i in range(3):
         if ((newCombo[i][0] == '0') and len(newCombo[i]) > 1):
             return(False)
-
+    
+    #check validity using numb function
     if numb(newCombo[0]) + numb(newCombo[1]) != numb(newCombo[2]):
         return(False)
     
     return(True)
-    
+
+#creats the integer value for the list of nmbers (ex. it converts [1, 2, 3] into 123) 
 def numb(arr):
     
     num = 0
